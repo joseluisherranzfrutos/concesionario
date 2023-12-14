c39a70ba8f47b459f63cac6330a8b0c91899358e
patricia-arisan <patricia.ariza1331@gmail.com>
2023-12-14 14:07:41 +0100

    dml empleados

--- dml.sql
+++ dml.sql
@@ -2,3 +2,7 @@ INSERT INTO concesionario.cliente (nif, nombre, direccion, ciudad, telefono, cat
 VALUES
   ('12345678A', 'Juan Lopez', 'Calle Gran Vía, 123', 'Madrid', '123456789', 'Oro'),
   ('98765432B', 'Pedro Gonzalez', 'Calle Arganda, 20', 'Madrid', '987654321', 'Plata');
+  
+  INSERT INTO concesionarios.empleado (ID, NOMBRE, DIRECCION, EMAIL, TELEFONO, SUELDO, COMISION) VALUES 
+  (1, 'JUAN', 'CALLE GRAN VÍA, 51', 'juan@gmail.com', '916543876', 18000, 0.5),  (2, 'MARIA', 'CALLE ATOCHA, 13', 'maria@gmail.com', '643397543', 19000, 0.2);
+
