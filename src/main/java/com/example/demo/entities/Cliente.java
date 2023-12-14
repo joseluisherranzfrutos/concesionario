80e10b6f67183d925c99cb4b18d296e2ab5eb64f
minister <ministerp93@gmail.com>
2023-12-14 16:35:27 +0100

    cliente.entity

---
+++ src/main/java/com/example/demo/entities/Cliente.java
@@ -0,0 +1,69 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="DEPARTMENTS")
+public class Cliente {
+	
+	@Id
+	@Column(name="ID")
+	private Integer id;
+	@Column(name="NIF")
+	private String nif;
+	@Column(name="NOMBRE")
+	private String nombre;
+	@Column(name="DIRECCION")
+	private String direccion;
+	@Column(name="TELEFONO")
+	private String telefono;
+	@Column(name="CATEGORIA")
+	private String categoria;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNif() {
+		return nif;
+	}
+	public void setNif(String nif) {
+		this.nif = nif;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", nif=" + nif + ", nombre=" + nombre + ", direccion=" + direccion + ", telefono="
+				+ telefono + ", categoria=" + categoria + "]";
+	}
+	
+	
+	
+}
