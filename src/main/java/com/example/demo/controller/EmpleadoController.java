8b1b93133dabc063a5a1e2ed24a8cb348c41e785
patricia-arisan <patricia.ariza1331@gmail.com>
2023-12-14 15:48:43 +0100

    pequeño cambio

--- src/main/java/com/example/demo/controller/EmpleadoController.java
+++ src/main/java/com/example/demo/controller/EmpleadoController.java
@@ -1,5 +1,13 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/empleados")
+
 public class EmpleadoController {
+	
+	
 
 }
