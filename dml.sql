4f9a15851eb76cbc7f01b7399274c79b459a9037
Jose <joseluishf@gmail.com>
2023-12-14 13:57:59 +0100

    dml cliente

---
+++ dml.sql
@@ -0,0 +1,4 @@
+INSERT INTO concesionario.cliente (nif, nombre, direccion, ciudad, telefono, categoria)
+VALUES
+  ('12345678A', 'Juan Lopez', 'Calle Gran Vía, 123', 'Madrid', '123456789', 'Oro'),
+  ('98765432B', 'Pedro Gonzalez', 'Calle Arganda, 20', 'Madrid', '987654321', 'Plata');
