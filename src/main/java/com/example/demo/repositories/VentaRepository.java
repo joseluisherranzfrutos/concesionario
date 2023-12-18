cfdbca64ae65ac482c15003d8f7b86982c62fb51
Jose <joseluishf@gmail.com>
2023-12-18 12:38:36 +0100

    fix pk integer

--- src/main/java/com/example/demo/repositories/VentaRepository.java
+++ src/main/java/com/example/demo/repositories/VentaRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entities.Venta;
 
-public interface VentaRepository extends JpaRepository<Venta, String>{
+public interface VentaRepository extends JpaRepository<Venta, Integer>{
 
 }
