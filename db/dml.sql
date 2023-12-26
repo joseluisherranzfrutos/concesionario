f9eeb12293a6eb4ba478ea508e739c81f917a2b7
Jose <joseluishf@gmail.com>
2023-12-26 16:10:55 +0100

    generar venta front y dml distintos nombres

--- db/dml.sql
+++ db/dml.sql
@@ -4,7 +4,7 @@ VALUES
   ('98765432B', 'Pedro Gonzalez', 'Calle Arganda, 20', 'Madrid', '987654321', 'Plata',1,0);
   
 INSERT INTO concesionario.empleado (ID, NOMBRE, DIRECCION, EMAIL, TELEFONO, SUELDO, COMISION) VALUES 
-  (1, 'JUAN', 'CALLE GRAN VÍA, 51', 'juan@gmail.com', '916543876', 18000, 0.5),  (2, 'MARIA', 'CALLE ATOCHA, 13', 'maria@gmail.com', '643397543', 19000, 0.2);
+  (1, 'Javier Fernandez', 'CALLE GRAN VÍA, 51', 'juan@gmail.com', '916543876', 18000, 0.5),  (2, 'Maria Sanchez', 'CALLE ATOCHA, 13', 'maria@gmail.com', '643397543', 19000, 0.2);
 
 INSERT INTO concesionario.coche (`matricula`, `marca`, `modelo`, `color`, `precio`, `estado`, `anyo`)
 VALUES
